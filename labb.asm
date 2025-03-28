bf3faafe90f709d254291d991bf6fe53f19e552e
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-03-28 20:50:18 +0530

    labb.asm

--- labb.asm
+++ labb.asm
@@ -1,9 +1,9 @@
         AREA PROG,CODE,READONLY
 START
-	      LDR R0,=0x00000007
+	LDR R0,=0x00000007
       	LDR R1,=0x00000008
       	SUB R2,R1,R0
-	      SBC R3,R1,R0
+	SBC R3,R1,R0
       	RSB R4,R1,R0
       	BX LR
-	      END
+	END
